d1876dbd348e36af8580de2bbf3e7d776e47c5ac
deandimarzo <deandimarzo@gmail.com>
2022-10-04 07:44:16 -0400

    setting stuff up

---
+++ assets/preload/stages/factory.lua
@@ -0,0 +1,20 @@
+function onCreate()
+	-- background 
+
+	makeLuaSprite('factory-background', 'factory-background', -850, -1180);
+	setScrollFactor('factory-background', 1, 1);
+    scaleObject('factory-background', 1.1, 1.1);
+    
+    makeLuaSprite('factory-fore1', 'factory-fore1', 1240, 200);
+	setScrollFactor('factory-fore1', 1.2, 1);
+    scaleObject('factory-fore1', 1.2, 1.2);
+    
+    makeLuaSprite('factory-fore2', 'factory-fore2', -1040, 200);
+	setScrollFactor('factory-fore2', 1.3, 1);
+    scaleObject('factory-fore2', 1.2, 1.2);
+    
+	addLuaSprite('factory-background', false);
+    addLuaSprite('factory-fore1', true);
+    addLuaSprite('factory-fore2', true);
+
+end
